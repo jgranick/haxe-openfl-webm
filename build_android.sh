6747cd049513e3364eab31144ba32feed1d6d0a6
Joshua Granick <jgranick@users.noreply.github.com>
2017-06-27 14:05:29 -0700

    Remove outdated scripts

--- build_android.sh
+++
@@ -1,5 +0,0 @@
-mkdir ndll/Android 2> /dev/null
-pushd project
-haxelib run hxcpp Build.xml -Dandroid -v
-haxelib run hxcpp Build.xml -Dandroid -DHXCPP_ARMV7 -v
-popd
--- build_ios.sh
+++
@@ -1,5 +0,0 @@
-mkdir ndll/iPhone 2> /dev/null
-pushd project
-IPHONE_VER=6.0 haxelib run hxcpp Build.xml -Diphonesim
-IPHONE_VER=6.0 haxelib run hxcpp Build.xml -Diphoneos
-popd
